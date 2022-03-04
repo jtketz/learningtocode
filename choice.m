fdabe4a3d7493033fb8c39df8ee2b112d32b83db
jtketz <50502062+jtketz@users.noreply.github.com>
2022-03-03 19:45:22 -0500

    Create choice.m

---
+++ choice.m
@@ -0,0 +1,10 @@
+% What type of life do you wish to lead?
+today = ['It is binary, make the choice: Easy or Hard...? '];
+choice = input(today);
+if choice == 0
+    disp('Easy choices lead to a hard life.')
+elseif choice == 1
+    disp('Hard choices lead to an easy life.')
+else
+    disp('Its binary, make the choice.')
+end
